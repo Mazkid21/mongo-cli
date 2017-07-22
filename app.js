971801688270c9538157c27216d8dba5232d9204
Mazkid21 <zmaraziti@yahoo.com>
2017-07-22 15:49:20 -0600

    Task 2 complete

---
+++ app.js
@@ -0,0 +1,37 @@
+var mongo = require("mongodb").MongoClient;
+var prompt = require("prompt-sync")();
+var url = "mongodb://localhost:27017/retaurant_db";
+
+  //var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
+  //if(allChoice == "all"){
+  //collection.find().toArray(function(err, doc){
+  //     console.log(doc);
+  //   });
+  // } else {
+  //   console.log("Aw, you don't want to see the restaurants?");
+  // }
+
+//TASK 1
+   // mongo.connect(url, function(err, db){
+   // var collection = db.collection('restaurants');
+ 
+   // var specific = prompt("Enter name of restaurant you are looking for' names: ");
+   //   	collection.find({"name":specific}).toArray(function (err, doc) {
+   //   		console.log(doc);
+   //   	});
+
+   //  });
+
+   // TASK 2
+
+//    mongo.connect(url, function(err, db){
+//   	var collection = db.collection('restaurants');
+//   	var add = prompt("Enter name of restaurant to add' name:");
+//   	var addAdress = prompt("Enter new' adress:");
+//   		collection.insert({
+//   			"name":add, 
+//   			"adress":addAdress}, function (er, doc){
+//   			console.log(doc);
+//   		});
+// });
+
